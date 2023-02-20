2b4519917ad919934e4920a86a6ff7499bbbfdeb
serkanursavas <serkan.ursavas@icloud.com>
2023-02-21 02:43:47 +0300

    preparing fundamentals

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [{ index: true, element: <Home /> }]
+  }
+])
+
 function App() {
-  return <div></div>
+  return <RouterProvider router={router} />
 }
 
 export default App
