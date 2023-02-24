86f2c64e383551143c8b405dbff2cb309201871d
serkanursavas <serkan.ursavas@icloud.com>
2023-02-25 01:44:23 +0300

    recorrection of menu

--- src/components/UI/LayoutCard.jsx
+++ src/components/UI/LayoutCard.jsx
@@ -1,6 +1,6 @@
 const LayoutCard = props => {
   return (
-    <div className="bg-background min-h-[100vh] px-5 pt-5 pb-9  md:bg-background md:flex md:justify-center md:items-center">
+    <div className="bg-background min-h-[100vh] px-5 pt-5 pb-9 overflow-hidden md:bg-background md:flex md:justify-center md:items-center">
       <div className="hidden md:block md:border md:border-white md:p-10 md:text-[36px] md:text-primary">
         We back as soon as possible !
       </div>
