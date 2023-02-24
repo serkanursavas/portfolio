95abdaa2abb57b98df2417b10d8fed4c5cfdca13
serkanursavas <serkan.ursavas@icloud.com>
2023-02-25 01:50:30 +0300

    recorrection of menu

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -9,7 +9,9 @@ const MainNavigation = props => {
   return (
     <div
       id="mainNav"
-      className={`${props.isOpen ? 'opacity-100' : 'opacity-0'}  bg-background w-full absolute left-0 px-5 h-[100vh]`}
+      className={`${
+        props.isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
+      }  bg-background w-full absolute left-0 px-5 h-auto`}
     >
       <ul className="mt-16 text-4xl space-y-9 font-light text-grey">
         <li>
