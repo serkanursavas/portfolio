1629feccdf0505a49b4eee7da7619bbfbdc76c25
serkanursavas <serkan.ursavas@icloud.com>
2023-02-26 03:23:26 +0300

    adding about route and fund.

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import PageTitle from '../components/PageTitle'
+
+const About = () => {
+  return (
+    <>
+      <PageTitle
+        title="about-me"
+        subtitle="Who am i?"
+      />
+    </>
+  )
+}
+
+export default About
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,16 +1,15 @@
 import ProjectItem from '../components/ProjectItem'
 
 import projects from '../store/projects'
+import PageTitle from '../components/PageTitle'
 
 const Works = () => {
   return (
     <>
-      <div className="mt-8 text-white">
-        <p className="font-medium text-[36px]">
-          <span className="text-primary">/</span>projects
-        </p>
-        <p className="mt-1 font-light">List of my projects</p>
-      </div>
+      <PageTitle
+        title="projects"
+        subtitle="List of my projects"
+      />
 
       <div className="mt-16 text-white">
         <h2 className="text-2xl">
