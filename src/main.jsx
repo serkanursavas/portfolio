fc8111fa0b815b4497a4d1e5b2f80ec9f13e2d21
serkanursavas <serkan.ursavas@icloud.com>
2023-12-13 22:53:17 +0300

    feat - project context

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import ProjectsContextProvider from './store/projects-context'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ProjectsContextProvider>
+      <App />
+    </ProjectsContextProvider>
   </React.StrictMode>
 )
---
+++ src/store/projects-context.jsx
@@ -0,0 +1,31 @@
+import { createContext, useEffect, useState } from 'react'
+
+export const ProjectsContext = createContext({
+  projects: []
+})
+
+function ProjectsContextProvider({ children }) {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    const getProjects = async () => {
+      const response = await fetch('http://localhost:3000/api/projects', {
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': import.meta.env.VITE_API_KEY
+        }
+      })
+      const data = await response.json()
+      setProjects(data.projects)
+    }
+    getProjects()
+  }, [])
+
+  const value = {
+    projects
+  }
+
+  return <ProjectsContext.Provider value={value}>{children}</ProjectsContext.Provider>
+}
+
+export default ProjectsContextProvider
